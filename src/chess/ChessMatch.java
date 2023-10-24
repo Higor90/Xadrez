7f94b62f3789d1f22f2ee4507e7e654f0b6902b4
Higor Nobrega <nobregahigor@gmail.com>
2023-10-24 18:03:48 -0300

    Pequena melhora na impressão das peças.

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -7,33 +7,40 @@ import chess.pieces.king;
 public class ChessMatch {
 
 	private Board board;
-	
+
 	public ChessMatch() {
 		board = new Board(8, 8);
 		InitialSetup();
-		}
-	
-	public ChessPiece[][] getPieces(){
+	}
+
+	public ChessPiece[][] getPieces() {
 		ChessPiece[][] mat = new ChessPiece[board.getRows()][board.getColumns()];
-		for (int i=0;i<board.getRows();i++) {
-			for (int j=0;j<board.getColumns();j++) {
-				mat[i][j] =  (ChessPiece) board.piece(i, j);
+		for (int i = 0; i < board.getRows(); i++) {
+			for (int j = 0; j < board.getColumns(); j++) {
+				mat[i][j] = (ChessPiece) board.piece(i, j);
 			}
 		}
 		return mat;
 	}
+
 	private void placeNewPiece(char column, int row, ChessPiece piece) {
-		board.placePice(piece,new ChessPosition(column,row).toPosition());
-	}
-	
-	
-	private void InitialSetup() {
-		placeNewPiece('b', 6,new Rook(board, Color.WHITE));
-		placeNewPiece('e', 8,new king(board, Color.BLACK));
-		placeNewPiece('e', 1,new king(board, Color.BLACK));
-		
-		 
+		board.placePice(piece, new ChessPosition(column, row).toPosition());
 	}
 
-	
+	private void InitialSetup() {
+		placeNewPiece('c', 1, new Rook(board, Color.WHITE));
+        placeNewPiece('c', 2, new Rook(board, Color.WHITE));
+        placeNewPiece('d', 2, new Rook(board, Color.WHITE));
+        placeNewPiece('e', 2, new Rook(board, Color.WHITE));
+        placeNewPiece('e', 1, new Rook(board, Color.WHITE));
+        placeNewPiece('d', 1, new king(board, Color.WHITE));
+
+        placeNewPiece('c', 7, new Rook(board, Color.BLACK));
+        placeNewPiece('c', 8, new Rook(board, Color.BLACK));
+        placeNewPiece('d', 7, new Rook(board, Color.BLACK));
+        placeNewPiece('e', 7, new Rook(board, Color.BLACK));
+        placeNewPiece('e', 8, new Rook(board, Color.BLACK));
+        placeNewPiece('d', 8, new king(board, Color.BLACK));
+	}
+
 }
--- src/chess/pieces/king.java
+++ src/chess/pieces/king.java
@@ -4,16 +4,16 @@ import boardgame.Board;
 import chess.ChessPiece;
 import chess.Color;
 
-public class king extends ChessPiece{
+public class king extends ChessPiece {
 
 	public king(Board board, Color color) {
 		super(board, color);
-		
+
 	}
 
 	@Override
 	public String toString() {
 		return "K";
 	}
-	
+
 }
