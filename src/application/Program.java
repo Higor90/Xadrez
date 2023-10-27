df1108dec3acffc9317b382dd62c2db7b12641c0
Higor Nobrega <nobregahigor@gmail.com>
2023-10-27 15:27:56 -0300

    Movimento especial - Promoção.

--- src/application/Program.java
+++ src/application/Program.java
@@ -11,44 +11,49 @@ import chess.ChessPiece;
 import chess.ChessPosition;
 
 public class Program {
- 
+
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		ChessMatch chessMatch = new ChessMatch();
 		List<ChessPiece> captured = new ArrayList();
-		
-		while(!chessMatch.getcheckMate()) {
-				try {
+
+		while (!chessMatch.getcheckMate()) {
+			try {
 				UI.clearScreen();
 				UI.printMatch(chessMatch, captured);
 				System.out.println();
 				System.out.print("Source: ");
 				ChessPosition source = UI.readChessPosition(sc);
-				
+
 				boolean[][] possibleMoves = ChessMatch.possibleMoves(source);
 				UI.clearScreen();
-				//Esse erro
-				UI.printBoard(ChessMatch.getPieces(),possibleMoves);
+				// Esse erro
+				UI.printBoard(ChessMatch.getPieces(), possibleMoves);
 				System.out.println();
 				System.out.print("Target: ");
 				ChessPosition target = UI.readChessPosition(sc);
-				
+
 				ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+
+				if (capturedPiece != null) {
+					captured.add(capturedPiece);
+				}
 				
-				if(capturedPiece != null) {
-					captured.add(capturedPiece); 
+				if(chessMatch.getPromoted() != null) {
+					System.out.print("Entre com a peça para promoção (B/N/R/Q)");
+					String type = sc.nextLine();
+					chessMatch.replacePromotedPiece(type);
 				}
-				}
-				catch (ChessException e){
-					System.out.println(e.getMessage());
-					sc.nextLine();
-				}				
-				catch (InputMismatchException e){
-					System.out.println(e.getMessage());
-					sc.nextLine();
-				}	
+				
+			} catch (ChessException e) {
+				System.out.println(e.getMessage());
+				sc.nextLine();
+			} catch (InputMismatchException e) {
+				System.out.println(e.getMessage());
+				sc.nextLine();
 			}
-			UI.clearScreen();
-			UI.printMatch(chessMatch, captured);
-		 }
+		}
+		UI.clearScreen();
+		UI.printMatch(chessMatch, captured);
+	}
 }
